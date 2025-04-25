04f7338d8b254c3125a8e93b90c4ec2f9b133c61
HeftySammich <138090485+HeftySammich@users.noreply.github.com>
2025-04-25 10:15:03 -0400

    updated project with working main.js and UI

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
+import commonjs from 'vite-plugin-commonjs';
 
 export default defineConfig({
+  plugins: [commonjs()],
   resolve: {
     alias: {
-      "@hashgraph/hedera-wallet-connect": "@hashgraph/hedera-wallet-connect/dist/lib/dapp/index.js",
+      '@hashgraph/hedera-wallet-connect': '@hashgraph/hedera-wallet-connect/dist/lib/dapp/index.js',
     },
   },
   optimizeDeps: {
