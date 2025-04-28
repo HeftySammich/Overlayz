f02538da1e86a26d60151ec2f86030e355318701
HabeebullahOpe <Opeyemihabeebullah279@gmail.com>
2025-04-28 08:28:43 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
-import commonjs from 'vite-plugin-commonjs';
 
 export default defineConfig({
-  plugins: [commonjs()],
   resolve: {
     alias: {
-      '@hashgraph/hedera-wallet-connect': '@hashgraph/hedera-wallet-connect/dist/lib/dapp/index.js',
+      "@hashgraph/hedera-wallet-connect": "@hashgraph/hedera-wallet-connect/dist/lib/dapp/index.js",
     },
   },
   optimizeDeps: {
